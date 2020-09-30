11c0f04204f84827ca4687a30646651e8a5af7ec
prerna <bhatraprerna061@gmail.com>
2020-09-30 11:11:59 +0530

    fetchtasks

--- src/task.js
+++ src/task.js
@@ -3,17 +3,26 @@ import './App.css';
 import * as axios from 'axios';
 class Task extends React.Component
 {
-  state={ advice:''};
+  state={ name:'',task:'',exp:''};
     componentDidMount()
     {
-      this.fetchAdvice();
+      this.fetchtask();
     }
-     fetchAdvice=()=>{
+     fetchtask=()=>{
       axios.get('https://infinite-chamber-35629.herokuapp.com/list')
       .then((response)=>
       {
-       const data=response.data;
-       console.log(data);
+
+       const name=response.data[0].name;
+        const task=response.data[0].task;
+        const exp=response.data[0].dur;
+        console.log(response.data)
+        console.log(name);
+        console.log(task);
+        console.log(exp);
+		 this.setState({name:name})
+		 this.setState({task:task})
+		 this.setState({exp:exp})
    
       }).catch((error)=>
       {
@@ -22,11 +31,15 @@ class Task extends React.Component
     }
   render()
   {
-    const {data}=this.state;
+    const {name}=this.state;
+    const {task}=this.state;
+     const {exp}=this.state;
     return(
      <div className="app">
        <div className="card">
-       <h1 className="heading">{data}</h1>
+       <h1 className="heading">Name:{name}</h1>
+       <h1 className="heading">Task:{task}</h1>
+       <h1 className="heading">Expires on:{exp}</h1>
        </div>  
      </div>
      
